1f907f10e110fcde0ef5db12f39026004acab199
SergeyMitkin <sergeymitkin@gmail.com>
2022-05-20 08:07:39 +0300

    изменён пароль для php-mailer

--- models/m_tasks.php
+++ models/m_tasks.php
@@ -100,7 +100,7 @@ function sendEmail($message){
         $mail->Host       = 'smtp.mail.ru';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
         $mail->Username   = 'li-mukhammed@mail.ru';                     // SMTP username
-        $mail->Password   = '7qfnj8Mz9ATSyNB';                               // SMTP password
+        $mail->Password   = '2pUuKIeF2ia$';                               // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
         $mail->CharSet = "utf-8";
